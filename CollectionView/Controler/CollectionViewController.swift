c6c97fcdb5bcfd533b0bed10524790c7d8481cc2
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2019-08-10 10:48:53 +0200

    Set MVC pattern organization and add collectionView

---
+++ CollectionView/Controler/CollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewController.swift
+//  CollectionView
+//
+//  Created by Janin Culhaoglu on 10/08/2019.
+//  Copyright © 2019 Janin Culhaoglu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CollectionView/View/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Janin Culhaoglu on 10/08/2019.
+//  Copyright © 2019 Janin Culhaoglu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
